e0b0c40aae0945066b231eb8f70d857e56052dc0
Zero <405686773@qq.com>
2019-05-08 14:48:52 +0800

    增加日志打印，使用模拟headers和代理Ip反爬虫机制

--- 6.yinyuetai-anti-crawler/yinyuetai.py
+++ 6.yinyuetai-anti-crawler/yinyuetai.py
@@ -11,16 +11,18 @@
 import requests
 import bs4
 import random
+import traceback
 
 
 def get_html(url):
     try:
-        r = requests.get(url, timeout=30)
+        r = requests.get(url, timeout=30, headers=get_agent(), proxies=get_proxy())
         r.raise_for_status
         r.encoding = r.apparent_encoding
         return r.text
-    except:
-        return "Someting Wrong！"
+    except Exception as e:
+        traceback.print_exc()
+        return "error"
 
 
 def get_agent():
@@ -31,25 +33,28 @@ def get_agent():
     agents = ['Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;',
               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv,2.0.1) Gecko/20100101 Firefox/4.0.1',
               'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
-              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
               'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)']
     fakeheader = {}
-    fakeheader['User-agent'] = agents[random.randint(0, len(agents))]
+    # random.randint包上且包下，即random.randint(0, 10)，可以随机0-10的数字，包括0和10
+    fakeheader['User-agent'] = agents[random.randint(0, len(agents)-1)]
     return fakeheader
 
 
 def get_proxy():
     '''
-    简答模拟代理池
+    简单模拟代理池(以下皆是免费的ip代理，质量较差，容易失效，如遇ip失效或已被封请自行寻找)
+    http://www.gatherproxy.com/zh/ 从这个找国际的ip比较容易成功，国内的大部分都被封了
+    ip代理只是为了防止自己ip爬虫被网址封禁，所以用自己ip不用代理去爬，质量肯定会更好
     返回一个字典类型的键值对，
     '''
-    proxy = ["http://116.211.143.11:80",
-             "http://183.1.86.235:8118",
-             "http://183.32.88.244:808",
-             "http://121.40.42.35:9999",
-             "http://222.94.148.210:808"]
+    proxy = ["181.176.209.86:8080",
+             "190.102.154.131:8080",
+             "103.240.206.152:55740"]
+    random_ip = proxy[random.randint(0, len(proxy)-1)]
     fakepxs = {}
-    fakepxs['http'] = proxy[random.randint(0, len(proxy))]
+    fakepxs['http'] = 'http://' + random_ip
+    fakepxs['https'] = 'https://' + random_ip
     return fakepxs
 
 
@@ -68,6 +73,8 @@ def get_content(url):
 
     # 找到我们需要的每个li标签
     html = get_html(url)
+    if html == 'error':
+        print('网页获取失败')
     soup = bs4.BeautifulSoup(html, 'lxml')
     li_list = soup.find_all('li', attrs={'name': 'dmvLi'})
      
@@ -96,7 +103,6 @@ def main():
     for suff in suffix:
         url = base_url+suff
         get_content(url)
-
 
 if __name__ == '__main__':
     main()
