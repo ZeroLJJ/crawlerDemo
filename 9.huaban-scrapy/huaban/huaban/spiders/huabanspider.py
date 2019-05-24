d85b002409af91cfa4fe76e58f547dc3251f8c95
Zero <405686773@qq.com>
2019-05-24 17:48:30 +0800

    补充提交

--- 9.huaban-scrapy/huaban/huaban/spiders/huabanspider.py
+++ 9.huaban-scrapy/huaban/huaban/spiders/huabanspider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from huaban.items import HuabanItem
 
 
 class HuabanspiderSpider(scrapy.Spider):
@@ -7,7 +8,15 @@ class HuabanspiderSpider(scrapy.Spider):
     allowed_domains = ['huaban.com']
     start_urls = ['https://huaban.com/explore/rixirenx/']
 
-
+    item = HuabanItem()
 
     def parse(self, response):
-        pass
+        img_list = response.xpath('//*[@id="waterfall"]/div[contains(@class, "pin")]/@data-id')
+        for img_item in img_list:
+            url = 'https://huaban.com/pin/' + img_item
+            yield scrapy.Request(url, callback=self.post_page)
+
+    def post_page(self, response):
+        # images_url = response.xpath("//div[@id='entry-content']//img/@src").extract()
+        # self.item['image_urls'] = images_url
+        return self.item
